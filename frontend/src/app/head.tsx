16245790c984f551e5784bcbe7d1550e9e545c5b
mhmmdhandika <muhammadhandika78@gmail.com>
2023-01-26 21:24:02 +0700

    Initial setup

---
+++ frontend/src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Home | Shorter Link</title>
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta name='description' content='Short the link' />
+      <link rel='icon' href='/favicon.ico' />
+    </>
+  );
+}
