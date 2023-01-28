0c582900ded294490664da39f18402950628cb7a
mhmmdhandika <muhammadhandika78@gmail.com>
2023-01-28 11:09:48 +0700

    created excess section

---
+++ frontend/src/app/client-components/Shorter.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import { useState } from 'react';
+
+function Shorter() {
+  const [inputLink, setInputLink] = useState('');
+  const [formState, setFormState] = useState(false);
+  const [copyText, setCopyText] = useState({
+    isCopied: false,
+    text: '',
+  });
+
+  const baseAPI = 'https://api.shrtco.de/v2/';
+
+  return (
+    <section id='shorten' className='mt-32 py-3 bg-neutral-light'>
+      <div className='partial-base-style'>
+        <form className='-mt-20 bg-primary-dark-violet bg-shorten-mobile bg-no-repeat bg-right-top py-6 px-5 flex flex-col gap-5 justify-between items-center rounded-xl lg:bg-shorten-desktop lg:bg-cover lg:flex-row lg:py-10 lg:px-14'>
+          <div className='w-full relative mb-5 lg:mb-0'>
+            <input
+              type='url'
+              placeholder='Shorten a link here...'
+              className={`outline-none w-full px-5 py-3 rounded-lg border-2 border-white peer lg:rounded-xl lg:flex-auto ${
+                formState && 'invalid:border-secondary-red'
+              }`}
+              // value={inputLink}
+              // onChange={e => {
+              //   setFormState(true);
+              //   setInputLink(e.target.value);
+              // }}
+              required
+            />
+            <p
+              className={`invisible mt-1 absolute w-full justify-self-start text-sm text-secondary-red ${
+                formState && 'peer-invalid:visible'
+              }`}
+            >
+              Please add a link
+            </p>
+          </div>
+          <button
+            type='submit'
+            className='min-w-[200px] w-full primary-button border-2 border-primary-cyan rounded-lg lg:rounded-xl lg:flex-1'
+            // onClick={getShortLink}
+          >
+            Shorten it!
+          </button>
+        </form>
+        {/* {links.map((item, index) => {
+        return (
+          <div
+            className='copied-link bg-white flex flex-col gap-5 rounded-xl justify-between items-center my-5 py-6 px-9 lg:flex-row'
+            key={index}
+          >
+            <div className='prev-link'>{item.original_link}</div>
+            <div
+              className='shorted-link text-primary-cyan grow text-end'
+              onCopy={() => {
+                setCopyText({ ...copyText, isCopied: true });
+                setTimeout(() => {
+                  setCopyText({ ...copyText, isCopied: false });
+                }, 2000);
+              }}
+            >
+              {item.full_short_link}
+            </div>
+            <button
+              className={`copy-link primary-button w-full rounded-lg lg:rounded-xl lg:w-[120px] ${
+                copyText.text === item.full_short_link &&
+                'bg-primary-dark-violet text-white'
+              }`}
+              onClick={() => handleCopyClick(item.full_short_link)}
+            >
+              {copyText.text === item.full_short_link ? 'Copied!' : 'Copy'}
+            </button>
+          </div>
+        );
+      })} */}
+      </div>
+    </section>
+  );
+}
+export default Shorter;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,24 @@
 import Image from 'next/image';
-import IllustrationWorking from '../assets/images/illustration-working.svg';
+import Shorter from './client-components/Shorter';
+import excessList from '@/data/excessList.json';
 
 export default function Home() {
   return (
     <>
+      {/* Jumbotron */}
       <section
         role='jumbotron'
         className={`partial-base-style mt-8 mb-44 text-center flex flex-col gap-1 items-center lg:text-start lg:flex-row`}
       >
         <Image
-          src={IllustrationWorking}
+          src='/assets/images/illustration-working.svg'
+          width={400}
+          height={400}
           alt='Illustration working'
           className='-mr-44 sm:w-[800px] lg:order-last'
         />
         <div className='mt-6'>
-          <h1 className='grow text-neutral-very-dark-violet text-[2.5rem] font-bold leading-[3.7rem] lg:leading-[5rem] lg:text-7xl'>
+          <h1 className='grow text-neutral-very-dark-violet text-[2.5rem] font-extrabold leading-[3.7rem] lg:leading-[5rem] lg:text-7xl'>
             More than just shorter links
           </h1>
           <p className={`text-neutral-grayfish-violet my-4 text-xl lg:w-[70%]`}>
@@ -22,6 +26,47 @@ export default function Home() {
             links are performing.
           </p>
           <button className='my-4 primary-button'>Get Started</button>
+        </div>
+      </section>
+      {/* Shorter link section (client component) */}
+      <Shorter />
+      {/* Excess section */}
+      <section className='py-28 bg-neutral-light'>
+        <div className='partial-base-style'>
+          <div className='mb-16 text-center'>
+            <h1 className='font-bold text-4xl text-primary-dark-violet'>
+              Advanced Statistics
+            </h1>
+            <p className='text-neutral-grayfish-violet w-full mx-auto my-4 lg:w-[400px]'>
+              Track how your links are performing across the web with our
+              advanced statictics dashboard.
+            </p>
+          </div>
+          <div className='relative text-center flex flex-col justify-center items-baseline gap-20 lg:flex-row lg:gap-10 lg:h-72 lg:text-start'>
+            {excessList.map((item, index) => {
+              const cardPosition = ['self-start', 'self-center', 'self-end'];
+
+              return (
+                <article
+                  key={index}
+                  className={`w-full z-20 bg-white p-11 rounded-lg ${cardPosition[index]} lg:w-auto`}
+                >
+                  <div className='-mt-20 flex justify-center lg:justify-start'>
+                    <div className='bg-primary-dark-violet w-[80px] h-[80px] rounded-[50%] flex items-center justify-center'>
+                      <img src={item.icon} alt='Track icon' />
+                    </div>
+                  </div>
+                  <h1 className='my-4 text-neutral-very-dark-violet font-semibold text-xl'>
+                    {item.name}
+                  </h1>
+                  <p className='text-base text-neutral-grayfish-violet'>
+                    {item.text}
+                  </p>
+                </article>
+              );
+            })}
+            <div className='absolute left-1/2 -translate-x-1/2 w-2 h-full bg-primary-cyan lg:w-full lg:h-2 lg:top-1/2 lg:-translate-y-1/2'></div>
+          </div>
         </div>
       </section>
     </>
