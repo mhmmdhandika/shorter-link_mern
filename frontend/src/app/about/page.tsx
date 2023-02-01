0de461f99e9dc80e2b86d6898bdaf707a37f1e55
mhmmdhandika <muhammadhandika78@gmail.com>
2023-02-01 19:53:47 +0700

    add dummy content in about page

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,3 +1,21 @@
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <section className='py-20'>
+      <div className='partial-base-style text-center'>
+        <h1 className='text-5xl font-medium mb-5'>About Us</h1>
+        <p>
+          This website is a part of{' '}
+          <a
+            href='https://www.frontendmentor.io/challenges/url-shortening-api-landing-page-2ce3ob-G/hub'
+            target='_blank'
+            className='text-primary-cyan underline'
+          >
+            Frontend Mentor
+          </a>{' '}
+          project. I've already this project, but I decided to improve like add
+          shorter page, see the short link info and etc.
+        </p>
+      </div>
+    </section>
+  );
 }
